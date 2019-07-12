ac381896bad2d5e2aaddbf73fb57a20bd49db080
Mikhail-Voronizkov <52339125+Mikhail-Voronizkov@users.noreply.github.com>
2019-07-12 10:35:35 +0700

    Update character.h

--- Project1/character.h
+++ Project1/character.h
@@ -1,4 +1,3 @@
-#pragma pack(1)
 #ifndef _CHARACTER_H_
 #define _CHARACTER_H_
 #include<iostream>
@@ -7,16 +6,22 @@ class word
 {
 public:
 	void readword(char* filename);
+	void copycontent(char* str);
 	void enscript(int mode);
 	bool compare(char* charac);
+	int lenght();
 	char* getcontent() const;
 	char* getenscript() const;
+	word();
 
 private:
 	char* m_content;
 	char* m_enscript;
 	char* m_token;
 };
+
+static int score, life;
+static char* playername;
 
 
 #endif
