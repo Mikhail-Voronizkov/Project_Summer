b18acea7beab99536fb3a2dc8bdedc1eb8caf66a
Mikhail-Voronizkov <52339125+Mikhail-Voronizkov@users.noreply.github.com>
2019-07-12 10:34:49 +0700

    Update character.cpp

--- Project1/character.cpp
+++ Project1/character.cpp
@@ -1,4 +1,5 @@
 #include"character.h"
+#include<string.h>
 
 using namespace std;
 
@@ -6,8 +7,48 @@ void word::readword(char* filename){
 
 }
 
-void word::enscript(int mode) {
+void word::copycontent(char* str) {
+	int len = strlen(str);
+	m_content = (char*)malloc(len * sizeof(char));
+	strcpy(m_content, str);
+}
 
+void word::enscript(int mode) {
+	int len = strlen(m_content);
+	m_enscript = (char*)malloc(len * sizeof(char));
+	strcpy(m_enscript, m_content);
+	if (len < 2)
+		return;
+	if (len == 2) {
+		char c = m_enscript[1];
+		m_enscript[1]= m_enscript[0];
+		m_enscript[0] = c;
+		return;
+	}
+	int ai = 0;
+	int bi = 0;
+	char* a = (char*)malloc(len - (len / 2) * sizeof(char));
+	char* b = (char*)malloc((len / 2) * sizeof(char));
+	for (int i = 0; i < len; i++) {
+		if (i % 2 == 0) {
+			*(a + ai) = *(m_content + i);
+			ai++;
+		}
+		else {
+			*(b + bi) = *(m_content + i);
+			bi++;
+		}
+	}
+	a[ai] = '\0';
+	b[bi] = '\0';
+	if (mode == 1) {
+		strcpy(m_enscript, b);
+		strcpy(m_enscript + bi, a);
+	}
+	else if(mode==2){
+		strcpy(m_enscript, a);
+		strcpy(m_enscript + ai, b);
+	}
 }
 
 bool word::compare(char* charac) {
@@ -15,13 +56,19 @@ bool word::compare(char* charac) {
 	return false;
 }
 
-char* word::getcontent() const {
+int word::lenght() {
+	int len = 0;
+	while (*m_content++)
+		len++;
+	return len;
+}
 
-	return NULL;
+char* word::getcontent() const {
+	return m_content;
 }
 
 char* word::getenscript() const {
-
-	return NULL;
+	return m_enscript;
 }
 
+word::word():m_content(NULL), m_enscript(NULL), m_token(NULL){}
